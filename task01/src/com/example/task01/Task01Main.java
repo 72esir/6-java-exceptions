2f5eaf5566e516351a24d333a9d93c09aed23870
Aleksey Salamatin <asalamatin640@gmail.com>
2024-10-20 22:43:14 +0500

    Salamatin PrE-202 Task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,14 +2,17 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
+
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         codeWithNPE();
-         */
+
     }
 
     static void codeWithNPE() {
+        String a = null;
+        a.getBytes();
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
